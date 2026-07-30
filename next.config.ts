7c57dde8a5b40c468dcebb38bacc5df58f8a92a9
chae-eunshin <chaeeun0217@naver.com>
2026-07-30 13:11:28 +0900

    chore: next.config에 s3버킷 추가 허용
    - 이미지 소스가 외부 호스트이면 직접 해당 호스트로 찾아가지 않고 next에 요청을 해서 next 옵티마이저가 서버사이드에서 원본을 get하기 때문에 핻당 호스트를 허용해줘야함.
    - 이미지 소스를 s3로 한정시켜서 해당 소스에서 받아오도록 함.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      // 문제 이미지 버킷(arn:aws:s3:::to-teacher-exam).
+      // 리전 엔드포인트와 리전 없는 구형 엔드포인트가 둘 다 나올 수 있어 함께 등록한다.
+      {
+        protocol: "https",
+        hostname: "to-teacher-exam.s3.*.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "to-teacher-exam.s3.amazonaws.com",
+      },
     ],
   },
   async headers() {
